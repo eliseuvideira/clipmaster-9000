a25feb0cd76f30ac929e0229116dde59695004c9
Eliseu Videira <eliseuvideira1@gmail.com>
2019-11-18 21:53:02 -0300

    Adding and removing from clipping menubar

--- app/renderer.js
+++ app/renderer.js
@@ -9,7 +9,7 @@ const createClippingElement = (clippingText) => {
   clippingElement.innerHTML = `
     <div class="clipping-text" disabled="true"></div>
     <div class="clipping-controls">
-      <button class="copy-clipping">&rar; Clipboard</button>
+      <button class="copy-clipping">&rarr; Clipboard</button>
       <button class="publish-clipping">Publish</button>
       <button class="remove-clipping">Remove</button>
     </div>
@@ -24,4 +24,36 @@ const addClippingToList = () => {
   clippingsList.prepend(clippingElement);
 };
 
+const getButtonParent = ({ target }) => {
+  return target.parentNode.parentNode;
+};
+
+const getClippingText = (clippingListItem) => {
+  return clippingListItem.querySelector('.clipping-text').innerText;
+};
+
+const removeClipping = (target) => {
+  target.remove();
+};
+
+const writeToClipboard = (clippingText) => {
+  clipboard.writeText(clippingText);
+};
+
 btnCopyFromClipboard.addEventListener('click', addClippingToList);
+
+clippingsList.addEventListener('click', (event) => {
+  const hasClass = (className) => event.target.classList.contains(className);
+
+  const clippingListItem = getButtonParent(event);
+
+  if (hasClass('remove-clipping')) {
+    removeClipping(clippingListItem);
+  }
+  if (hasClass('copy-clipping')) {
+    writeToClipboard(getClippingText(clippingListItem));
+  }
+  if (hasClass('publish-clipping')) {
+    console.log('publish clipping');
+  }
+});
